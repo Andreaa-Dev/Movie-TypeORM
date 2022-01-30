01e6c60e3092356262d0ebfb447fe9696359aed0
Andrea <ngocanhhsgs@gmail.com>
2022-01-30 14:28:25 +0200

    set up passport

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -1,8 +1,11 @@
 import GoogleTokenStrategy from 'passport-google-id-token'
+import {Strategy as JwtStrategy, ExtractJwt} from 'passport-jwt'
 
+const clientId = process.env.GOOGLE_CLIENT_ID
 export const googleStrategy = new GoogleTokenStrategy (clientID) 
 async function (parsedToken:any, googleId: string, done: any ){
 const userPayload= {
-    email: pasrsedToken.payload.email
+    email: pasrsedToken.payload.email,
+
 }
 }
