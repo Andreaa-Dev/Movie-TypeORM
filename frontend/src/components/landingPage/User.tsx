e83c535bdf280c477227aad8bf6f7b231480d125
Andrea <ngocanhhsgs@gmail.com>
2022-01-31 22:57:45 +0200

    add google log in frontend

--- frontend/src/components/landingPage/User.tsx
+++ frontend/src/components/landingPage/User.tsx
@@ -4,18 +4,7 @@ import { profileIcon } from "../list/profileIcon";
 import { BoxColumn, BoxRow, Text } from "../style";
 
 function User() {
-  return (
-    <BoxRow>
-      {profileIcon.map((item) => {
-        return (
-          <BoxColumn>
-            <img src={item.link} alt={item.name} height="144px" width="144px" />
-            <Text>{item.name}</Text>
-          </BoxColumn>
-        );
-      })}
-    </BoxRow>
-  );
+  return <BoxRow></BoxRow>;
 }
 
 export default User;
---
+++ frontend/src/components/user/GoogleLogInPage.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import axios from "axios";
+import GoogleLogIn from "react-google-login";
+import GoogleIcon from "@mui/icons-material/Google";
+
+import { ButtonStyle } from "../style";
+
+const clientId =
+  "1052223541956-dhv3nmo7tc3rbmusl92subblp9oc30hs.apps.googleusercontent.com";
+function GoogleLogInPage() {
+  const dataGoogle = async (response: any) => {
+    let res = await axios.post("/user/google-authenticate", {
+      id_token: response,
+    });
+  };
+
+  return (
+    <div>
+      <GoogleLogIn
+        clientId={clientId}
+        render={(renderProps) => (
+          <ButtonStyle
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+          >
+            <GoogleIcon />
+          </ButtonStyle>
+        )}
+        buttonText=""
+        onSuccess={dataGoogle}
+        onFailure={dataGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+    </div>
+  );
+}
+
+export default GoogleLogInPage;
